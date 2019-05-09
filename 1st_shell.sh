3d0b08e649f96adfe5ec828d4aa7de56e3a248f0
rocksolid911 <siddhantsaraf.91@gmail.com>
2019-05-10 01:07:55 +0530

    1st comit

---
+++ 1st_shell.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#echo "enter names: "
+#read name1 name2 name3
+#echo $name1 $name2 $name3
+read -p 'username:' user_v
+echo "username:"$user_v
+read -sp 'password:' pass_v
+echo $pass_v
---
+++ hello.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "hello world"
+echo "i am siddhant"
+#its a comment
+echo $BASH
+echo $PWD
+echo $HOME
+name=siddhant
+echo $name
